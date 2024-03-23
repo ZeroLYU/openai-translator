2e14efc64582c5b15389abd03a17f197508ea4ee
ZeroLYU <162791094+ZeroLYU@users.noreply.github.com>
2024-03-23 16:41:13 +0800

    Add files via upload

--- ai_translator/utils/argument_parser.py
+++ ai_translator/utils/argument_parser.py
@@ -1,10 +1,11 @@
 import argparse
-
 class ArgumentParser:
     def __init__(self):
         self.parser = argparse.ArgumentParser(description='Translate English PDF book to Chinese.')
-        self.parser.add_argument('--config', type=str, default='config.yaml', help='Configuration file with model and API settings.')
-        self.parser.add_argument('--model_type', type=str, required=True, choices=['GLMModel', 'OpenAIModel'], help='The type of translation model to use. Choose between "GLMModel" and "OpenAIModel".')        
+        self.parser.add_argument('--config', type=str, default='config.yaml', help='Configuration file with model '
+                                                                                    'and API settings.')
+        self.parser.add_argument('--model_type', type=str, choices=['GLMModel', 'OpenAIModel'],
+                                 help='The type of translation model to use. Choose between "GLMModel" and "OpenAIModel".')
         self.parser.add_argument('--glm_model_url', type=str, help='The URL of the ChatGLM model URL.')
         self.parser.add_argument('--timeout', type=int, help='Timeout for the API request in seconds.')
         self.parser.add_argument('--openai_model', type=str, help='The model name of OpenAI Model. Required if model_type is "OpenAIModel".')
@@ -14,6 +15,12 @@ class ArgumentParser:
 
     def parse_arguments(self):
         args = self.parser.parse_args()
+        print(args)
         if args.model_type == 'OpenAIModel' and not args.openai_model and not args.openai_api_key:
             self.parser.error("--openai_model and --openai_api_key is required when using OpenAIModel")
+        if args.model_type == 'GLMModel' and not args.glm_model_url:
+            self.parser.error("--glm_model_url is required when using GLMModel")
         return args
+
+    def parse_args(self):
+        return self.parser.parse_args()
